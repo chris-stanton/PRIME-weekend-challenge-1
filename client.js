9e8ed9de44b29f483fafe154d0a5724b164f6709
Chris Stanton <chrisstanton@Chriss-MacBook-Pro.local>
2017-02-12 21:47:43 -0600

    updated styles.css

--- client.js
+++ client.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
     var jobTitle = $('#jobTitle').val();
     var annualSalary = $('#annualSalary').val();
 
-//Total monthly average salary calculation - *broken need to fix*
+//Total monthly average salary calculation
     var singleMonthlySalary = annualSalary / 12;
     $('#container').text('Monthly Salary Expenditures: ' + singleMonthlySalary);
 
@@ -19,12 +19,10 @@ $(document).ready(function(){
     $('#idNumberContainer').append('<p> </p>' + idNumber);
     $('#jobTitleContainer').append('<p> </p>' + jobTitle);
     $('#annualSalaryContainer').append('<p> </p>' + annualSalary);
+
 //Deletes input value as a line - *broken fuction needs to be fixed*
-    $('#firstNameContainer').on('click', '#deleteButton', function(){
-      $(this).remove().val();
+    $('#resultTable').on('click', '#deleteButton', function(){
+      $(this).closest('tr').remove().val();
     });
   });
 });
-
-
-//.parentsUntil("table")
